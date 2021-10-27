e5d86a50aeef5bf2f1328dfdba7203ab1da3139c
“[jose3216]” <[jose3216@stud.kea.dk]>
2021-10-27 14:48:36 +0200

    Før modal sættes ind

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
-console.clear();
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
-gsap.defaults({ ease: "none" });
+// Animation Welcome text
 
-const pulses = gsap.timeline({
-  defaults: {
-    duration: 0.05,
-    autoAlpha: 1,
-    scale: 2,
-    transformOrigin: "center",
-    ease: "elastic(2.5, 1)",
+gsap.from(".welcome_text", {
+  scaleX: 0,
+  stagger: 0.5,
+  duration: 1,
+  ease: Power3,
+  scrollTrigger: {
+    trigger: ".welcome",
+    start: "bottom bottom",
+    end: "top top",
+    // markers: true,
   },
 });
-  .to(".ball02, .text01", {}, 0.2)
-  .to(".ball03, .text02", {}, 0.33)
-  .to(".ball04, .text03", {}, 0.46);
-
-const main = gsap
-  .timeline({
-    defaults: { duration: 1 },
-    scrollTrigger: {
-      trigger: "#svg",
-      scrub: true,
-      start: "top center",
-      end: "bottom center",
-    },
-  })
-  .to(".ball02", {
-    duration: 0.01,
-    autoAlpha: 1,
-  })
-  .from(".theLine", { drawSVG: 0 }, 0)
-  .to(
-    ".ball01",
-    {
-      motionPath: {
-        path: ".theLine",
-        align: ".theLine",
-        alignOrigin: [0.5, 0.5],
-      },
-    },
-    0
-  )
-  .add(pulses, 0);
